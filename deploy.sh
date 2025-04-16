00691923ea18b83a283ec67e96e4bb549072ab83
LmeHW <124685722+LmeHW@users.noreply.github.com>
2025-04-15 20:31:28 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
+(sleep 3 && open http://localhost:5173) &
 
-# 1. 激活 Conda 环境
-echo "Activating Conda environment: newsnow"
-# 适用于 bash/zsh 环境
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate newsnow
-
-
-# 3. 自动在后台延迟打开浏览器
-(sleep 2 && open http://localhost:5173) &
-
-# 2. 启动 pnpm 开发服务器（前台运行）
+# 启动 pnpm 开发服务器（前台运行）
 echo "Starting pnpm dev server..."
 pnpm dev
 
